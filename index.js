78d5e8610fdc0c7bf12651c8705f2da61a3edb4d
marcelo.ribas <marcelo.ribas@alphadba.com>
2020-01-07 16:54:01 +0000

    Configurações iniciais

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'))
+app.use(express.static(path.join(__dirname, 'public')))
+app.get('/', (req, res) => {
+  res.render('home')
+})
+
+app.listen(8080, err => {
+  if (err) {
+    console.log('ConvertMyMoney Server is not running')
+  } else {
+    console.log('ConvertMyMoney Server is running')
+  }
+})
